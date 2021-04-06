5708771036a28fa6c1d92fcec6b10fde6a3987f8
SEFIANE-EL OUADGHIRI M'hamed <56483502+mhamedouadghiri@users.noreply.github.com>
2021-04-06 17:07:47 +0100

    feat(draft): ML model/Android integration -- tfrs doc template -- will improve latter

--- app/src/main/java/com/mhamed/mymoviecompanion/MainActivity.java
+++ app/src/main/java/com/mhamed/mymoviecompanion/MainActivity.java
@@ -1,9 +1,12 @@
 package com.mhamed.mymoviecompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.mhamed.mymoviecompanion.recommender.TestRecSys;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("TMDB_API_KEY", BuildConfig.TMDB_API_KEY);
+
+        startActivity(new Intent(this, TestRecSys.class));
     }
 }
